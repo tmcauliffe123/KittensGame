4ee044ebbf3128c98140c13e77923d41c1caa260
Jonathan Beverley <jonathan.beverley@gmail.com>
2021-02-03 21:29:40 -0500

    add a convenience loader

---
+++ autoload.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Autoload ScriptKitties
+// @namespace    http://sagefault.net/
+// @version      1.0
+// @description  See name
+// @match        http*://bloodrizer.ru/games/kittens/
+// @match        http*://kittensgame.com/web/
+// @match        http*://kittensgame.com/beta/
+// @match        http*://kittensgame.com/alpha/
+// @grant        none
+// ==/UserScript==
+window.addEventListener("load", function() {
+    var script=document.createElement('script');
+    script.src='https://cdn.jsdelivr.net/gh/JonathanBeverley/KittensGame/ScriptKitties.js';
+    document.body.appendChild(script);
+}, false);
