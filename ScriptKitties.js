tmcauliffe123/KittensGame

commit f920967111908139cf79de1bc9c41c7aaae09358

use faith rather than temples to gate autoPraise

ScriptKitties.js
@@ -658,7 +658,7 @@ SK.Tasks = class {
 
     // Auto praise the sun
     autoPraise(ticksPerCycle) {
-        if (this.model.auto.praise && game.bld.get('temple').val > 0) {
+        if (this.model.auto.praise && game.resPool.get("faith") > 0) {
             game.religion.praise();
         }
     }

commit 723fa343b24422c60e7c2dc873e600e4b6f02a96

fix autoPraise break introduced in f9209671

ScriptKitties.js
@@ -9,7 +9,7 @@ var SK = class {
     }
 
     bldTabChildren() {
-        var children = game.bldTab.children;
+        let children = game.bldTab.children;
         if (children.length === 0) children = game.bldTab.buttons;
         return children;
     }
@@ -658,7 +658,7 @@ SK.Tasks = class {
 
     // Auto praise the sun
     autoPraise(ticksPerCycle) {
-        if (this.model.auto.praise && game.resPool.get("faith") > 0) {
+        if (this.model.auto.praise && game.resPool.get("faith").value > 0) {
             game.religion.praise();
         }
     }
@@ -879,7 +879,7 @@ SK.Tasks = class {
             this.ensureContentExists('Religion');
             this.ensureContentExists('Time');
 
-            // TODO: special case for Markers and Tears
+            // TODO: special case for Markers and Tears -- exists in autoUnicorn
             const tb = this.model.timeBuildings;
             for (const buttons of buttonGroups) {
                 if (buttons) {